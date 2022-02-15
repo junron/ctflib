c1a92feed56f5e77f46aca3f8d026a0c450edfcd
Lam Jun Rong <junron1@outlook.com>
2022-02-15 09:27:49 +0800

    Fix path bug

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,9 +1,9 @@
-import {fetchJSON} from "@/api/index";
+import {fetchJSON, postJSON} from "@/api/index";
 import {Response} from "@/types/response";
 import {User} from "@/types/user";
 
 export async function login(username: string, password: string): Promise<Response<User>> {
-  return fetchJSON("/login", {username, password});
+  return postJSON("/login", {username, password});
 }
 
 export async function register(username: string,
@@ -11,13 +11,13 @@ export async function register(username: string,
                                email: string,
                                githubUsername: string,
                                secret: string): Promise<Response<null>> {
-  return fetchJSON("/register", {username, password, email, github: githubUsername, secret});
+  return postJSON("/register", {username, password, email, github: githubUsername, secret});
 }
 
 export async function me(): Promise<Response<User>> {
-  return (await fetch("/me")).json();
+  return fetchJSON("/me");
 }
 
 export async function logout(): Promise<Response<null>> {
-  return fetchJSON("/logout", {});
+  return postJSON("/logout", {});
 }
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,7 +4,7 @@ export const apiRoot = location.hostname == "localhost"
   ? location.origin
   : "https://api.ctflib.junron.dev";
 
-export function fetchJSON<T>(path: string, data: any): Promise<Response<T>> {
+export function postJSON<T>(path: string, data: any): Promise<Response<T>> {
   return fetch(apiRoot + path, {
     method: "POST",
     headers: {
@@ -12,5 +12,12 @@ export function fetchJSON<T>(path: string, data: any): Promise<Response<T>> {
     },
     body: JSON.stringify(data),
     credentials: "include",
+  }).then((res) => res.json());
+}
+
+
+export function fetchJSON<T>(path: string): Promise<Response<T>> {
+  return fetch(apiRoot + path, {
+    method: "GET",
   }).then((res) => res.json());
 }
