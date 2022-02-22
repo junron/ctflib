351b7513e8ca5ac0843c38983fd5065a46a5e5a8
Lam Jun Rong <junron1@outlook.com>
2022-02-22 17:51:09 +0800

    Fix connection dropping issue

--- server/src/database/connect.ts
+++ server/src/database/connect.ts
@@ -1,17 +1,25 @@
 import {Connection, createConnection} from 'mysql2/promise';
 import config from "../config";
 
-let connection: any;
-
+let connection: Connection;
 
 export default async function getConnection(): Promise<Connection> {
-  if (connection) return connection;
-  connection = await createConnection({
-    host: 'localhost',
-    user: 'ctflib',
-    password: config.mysql_password,
-    database: 'ctflib',
-    multipleStatements: true
+  const closed = await new Promise(resolve => {
+    if (!connection) {
+      resolve(true);
+    } else {
+      connection.ping().catch(() => resolve(true)).then(() => resolve(false));
+    }
   });
+  if (closed) {
+    console.log("new connection");
+    connection = await createConnection({
+      host: 'localhost',
+      user: 'ctflib',
+      password: config.mysql_password,
+      database: 'ctflib',
+      multipleStatements: true
+    });
+  }
   return connection;
 };
